42e6dde5c3cb869c349e18c33d9eaae729c133d8
katarinasokcevic <katarina.sokcevic.vk@gmail.com>
2024-02-20 20:52:40 +0100

    users added on leaderboard

--- lib/pages/leaderboard.dart
+++ lib/pages/leaderboard.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:http/http.dart' as http;
@@ -16,24 +17,45 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
   @override
   void initState() {
     super.initState();
-    getLeaderboardData();
   }
 
-  void getLeaderboardData() async {
-
+  Future<List<Map<String, dynamic>>> fetchData(String userId, String language) async {
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference ref = storage.ref('$userId/${language}_results.txt');
+      String downloadURL = await ref.getDownloadURL();
+      final response = await http.get(Uri.parse(downloadURL));
+      List<String> lines = response.body.split('\n');
+      List<Map<String, dynamic>> data = [];
+      for (var line in lines) {
+        List<String> parts = line.split(':');
+        if (parts.length == 2) {
+          String key = parts[0].trim();
+          String value = parts[1].trim();
+          if (key == 'Time taken') {
+            data.add({
+              'userId': userId,
+              'timeTaken': double.parse(value.split(' ')[0]),
+            });
+          } else if (key == 'Correct answers') {
+            data.last['correctAnswers'] = int.parse(value);
+          }
+        }
+      }
+      return data;
+    } on http.ClientException catch (e) {
+      print('HTTP error: ${e.message}');
+      return [];
+    }
   }
 
-
+  String language = 'croatian';
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> leaderboardData = List.generate(10, (index) {
-      return {
-        'userId': 'user${index + 1}', // Generate user IDs like 'user1', 'user2', etc.
-        'correctAnswers': index * 5, // Randomly assign correct answers (for demonstration)
-        'timeTaken': index * 10, // Randomly assign time taken (for demonstration)
-      };
-    });
+    User? user = FirebaseAuth.instance.currentUser;
+    String userId = user!.uid;
+    List<Map<String, dynamic>> leaderboardData = [];
 
     return Scaffold(
       body: Center(
@@ -44,8 +66,11 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: () {
-                    // Handle button press
+                  onPressed: () async {
+                    language = 'croatian';
+                    leaderboardData = await fetchData(userId, language);
+                    leaderboardData.sort((a, b) => a['timeTaken'].compareTo(b['timeTaken']));
+                    setState(() {});
                   },
                   child: Text('Croatian'),
                   style: ElevatedButton.styleFrom(
@@ -56,10 +81,13 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                     padding: EdgeInsets.all(25),
                   ),
                 ),
-                SizedBox(width: 10), // Add some space between the buttons
+                SizedBox(width: 10),
                 ElevatedButton(
-                  onPressed: () {
-                    // Handle button press
+                  onPressed: () async {
+                    language = 'english';
+                    leaderboardData = await fetchData(userId, language);
+                    leaderboardData.sort((a, b) => a['timeTaken'].compareTo(b['timeTaken']));
+                    setState(() {});
                   },
                   child: Text('English'),
                   style: ElevatedButton.styleFrom(
@@ -72,34 +100,46 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                 ),
               ],
             ),
-            DataTable(
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Text('Ranking'),
-                ),
-                DataColumn(
-                  label: Text('Name'),
-                ),
-                DataColumn(
-                  label: Text('Correct\nanswers'),
-
-                ),
-                DataColumn(
-                  label: Text('Time'),
-                ),
-              ],
-              rows: leaderboardData.asMap().entries.map((entry) {
-                int index = entry.key;
-                Map<String, dynamic> playerData = entry.value as Map<String, dynamic>;
-                return DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text('${index + 1}.')), // Ranking data from 1 to 10
-                    DataCell(Text(playerData['userId'])), // User ID
-                    DataCell(Text('${playerData['correctAnswers']}')), // Correct answers
-                    DataCell(Text('${playerData['timeTaken']} seconds')), // Time taken
-                  ],
-                );
-              }).toList(),
+            FutureBuilder<List<Map<String, dynamic>>>(
+              future: fetchData(userId, language),
+              builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  List<Map<String, dynamic>> leaderboardData = snapshot.data!;
+                  leaderboardData.sort((a, b) => a['timeTaken'].compareTo(b['timeTaken']));
+                  return DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(
+                        label: Text('Ranking'),
+                      ),
+                      DataColumn(
+                        label: Text('Name'),
+                      ),
+                      DataColumn(
+                        label: Text('Correct\nanswers'),
+                      ),
+                      DataColumn(
+                        label: Text('Time'),
+                      ),
+                    ],
+                    rows: leaderboardData.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      Map<String, dynamic> playerData = entry.value as Map<String, dynamic>;
+                      return DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text('${index + 1}.')),
+                          DataCell(Text(playerData['userId'])),
+                          DataCell(Text('${playerData['correctAnswers']}')),
+                          DataCell(Text('${playerData['timeTaken']} seconds')),
+                        ],
+                      );
+                    }).toList(),
+                  );
+                }
+              },
             ),
             SizedBox(width: 30),
             ElevatedButton(
