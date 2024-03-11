491f019765a4e08871adf7fffecf09f7afbb964e
katarinasokcevic <katarina.sokcevic.vk@gmail.com>
2024-03-11 13:57:51 +0100

    circular progress indicator added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,86 +30,88 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return BaseScaffold(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: Text(
-              "The game Stroop effect",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Image.asset(
-            'assets/logo.png',
-            height: 180,
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-              ),
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    bool isMobile = constraints.maxWidth > 600;
-                    double fontSize = isMobile ? 23 : 16;
-
-                    return Text(
-                      "This application delves into the phenomenon known as the Stroop effect, an intriguing cognitive occurrence involving the interplay of colors and words in the brain.\n"
-                      "Objective is to observe user interactions, garner insights into cognitive processes, and contribute to a comprehensive understanding of cross-cultural influences on the Stroop effect.\n"
-                      "All acquired data will be meticulously documented and disseminated, aiming \n to enrich the collective understanding of cognitive phenomena.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: fontSize,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.black,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => buildDialog(context),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.pink,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 20,
-                ),
-              ),
-              child: const Text(
-                "Start",
+    return SafeArea(
+      child: BaseScaffold(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 2.0),
+              child: Text(
+                "The game Stroop effect",
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-        ],
+            Image.asset(
+              'assets/logo.png',
+              height: 180,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                      bool isMobile = constraints.maxWidth > 600;
+                      double fontSize = isMobile ? 23 : 16;
+
+                      return Text(
+                        "This application delves into the phenomenon known as the Stroop effect, an intriguing cognitive occurrence involving the interplay of colors and words in the brain.\n"
+                        "Objective is to observe user interactions, garner insights into cognitive processes, and contribute to a comprehensive understanding of cross-cultural influences on the Stroop effect.\n"
+                        "All acquired data will be meticulously documented and disseminated, aiming \n to enrich the collective understanding of cognitive phenomena.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => buildDialog(context),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 20,
+                  ),
+                ),
+                child: const Text(
+                  "Start",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -262,7 +264,21 @@ class _HomePageState extends State<HomePage> {
               fontWeight: FontWeight.bold,
             ),
           ),
-          content: Image.asset('assets/screenshot.jpg'),
+          content: Image.network(
+            'assets/screenshot.jpg',
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
+          ),
           actions: [
             TextButton(
               onPressed: () {
--- lib/pages/leaderboard.dart
+++ lib/pages/leaderboard.dart
@@ -4,7 +4,6 @@ import '../base_scaffold.dart';
 import 'home.dart';
 
 class LeaderboardPage extends StatefulWidget {
-
   @override
   _LeaderboardPageState createState() => _LeaderboardPageState();
 }
@@ -12,152 +11,168 @@ class LeaderboardPage extends StatefulWidget {
 class _LeaderboardPageState extends State<LeaderboardPage> {
   List<Map<String, dynamic>> allData = [];
   bool isCroatian = true;
+  late Future<void> fetchFuture;
 
   @override
   void initState() {
     super.initState();
-    fetchCroatianData();
+    fetchFuture = fetchCroatianData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BaseScaffold(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () async {
-                    isCroatian = true;
-                    fetchCroatianData();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pink,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+    return SafeArea(
+      child: BaseScaffold(
+        child: Center(
+          child: FutureBuilder(
+            future: fetchFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () async {
+                            isCroatian = true;
+                            fetchCroatianData();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.pink,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.all(25),
+                          ),
+                          child: const Text('Croatian'),
+                        ),
+                        const SizedBox(width: 10),
+                        ElevatedButton(
+                          onPressed: () async {
+                            isCroatian = false;
+                            fetchEnglishData();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.pink,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.all(25),
+                          ),
+                          child: const Text('English'),
+                        ),
+                      ],
                     ),
-                    padding: const EdgeInsets.all(25),
-                  ),
-                  child: const Text('Croatian'),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () async {
-                    isCroatian = false;
-                    fetchEnglishData();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pink,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(25),
-                  ),
-                  child: const Text('English'),
-                ),
-              ],
-            ),
-            DataTable(
-              columnSpacing: 30.0,
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Text(
-                    'Ranking',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Name',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Correct\nanswers',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Time',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-              rows: allData
-                  .asMap()
-                  .entries
-                  .map((entry) {
-                    int index = entry.key;
-                    Map<String, dynamic> playerData = entry.value;
-                    return DataRow(
-                      cells: <DataCell>[
-                        DataCell(
-                          Container(
-                            alignment: Alignment.center,
-                            child: Text('${index + 1}.'),
+                    DataTable(
+                      columnSpacing: 30.0,
+                      columns: const <DataColumn>[
+                        DataColumn(
+                          label: Text(
+                            'Ranking',
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                        DataCell(Text(playerData['name'])),
-                        DataCell(Container(
-                          alignment: Alignment.center,
-                          child: Text(
-                              '${isCroatian ? playerData['correctCroatian'] : playerData['correctEnglish']}'),
-                        )),
-                        DataCell(Text(
-                            '${isCroatian ? playerData['timeCroatian'] : playerData['timeEnglish']} s')),
+                        DataColumn(
+                          label: Text(
+                            'Name',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Correct\nanswers',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Time',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
                       ],
-                    );
-                  })
-                  .take(10)
-                  .toList(),
-
-            ),
-            const SizedBox(width: 30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
+                      rows: allData
+                          .asMap()
+                          .entries
+                          .map((entry) {
+                            int index = entry.key;
+                            Map<String, dynamic> playerData = entry.value;
+                            return DataRow(
+                              cells: <DataCell>[
+                                DataCell(
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: Text('${index + 1}.'),
+                                  ),
+                                ),
+                                DataCell(Text(playerData['name'])),
+                                DataCell(Container(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                      '${isCroatian ? playerData['correctCroatian'] : playerData['correctEnglish']}'),
+                                )),
+                                DataCell(Text(
+                                    '${isCroatian ? playerData['timeCroatian'] : playerData['timeEnglish']} s')),
+                              ],
+                            );
+                          })
+                          .take(10)
+                          .toList(),
+                    ),
+                    const SizedBox(width: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(25),
+                      ),
+                      child: const Text('Go to Menu'),
+                    ),
+                  ],
                 );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.pink,
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.all(25),
-              ),
-              child: const Text('Go to Menu'),
-            ),
-          ],
+              }
+            },
+          ),
         ),
       ),
     );
   }
 
-  void fetchCroatianData() async {
+  Future<void> fetchCroatianData() async {
     var db = FirebaseFirestore.instance;
     final resultsRef = db.collection("results");
-    final data = resultsRef.orderBy("correctCroatian", descending: true).
-    orderBy("timeCroatian", descending: false).limit(10);
+    final data = resultsRef
+        .orderBy("correctCroatian", descending: true)
+        .orderBy("timeCroatian", descending: false)
+        .limit(10);
     var snapshot = await data.get();
     allData = snapshot.docs.map((doc) => doc.data()).toList();
     setState(() {});
   }
 
-  void fetchEnglishData() async {
+  Future<void> fetchEnglishData() async {
     var db = FirebaseFirestore.instance;
     final resultsRef = db.collection("results");
-    final data = resultsRef.orderBy("correctEnglish", descending: true).
-    orderBy("timeEnglish", descending: false).limit(10);
+    final data = resultsRef
+        .orderBy("correctEnglish", descending: true)
+        .orderBy("timeEnglish", descending: false)
+        .limit(10);
     var snapshot = await data.get();
     allData = snapshot.docs.map((doc) => doc.data()).toList();
     setState(() {});
